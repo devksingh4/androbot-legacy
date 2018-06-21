ebafada7a65893694ff9ed970da2d4192cf3e981
Dev Singh <devksingh4@gmail.com>
2018-06-21 17:52:47 -0500

    rudimentary code for Music playin.

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ import os
 from chatterbot import ChatBot
 from discord.ext import commands
 from discord import Game, Embed
+from discord.voice_client import VoiceClient
 import asyncio as asyncio
 
 client = commands.Bot(command_prefix= '?')
+startup_extensions = ["Music"]
+
+if __name__ == "__main__":
+  for extension in startup_extensions:
+    try:
+      client.load_extension(extension)
+    except Exception as e:
+      exc = '{}: {}'.format(type(e).__name__, e)
+      print('Failed to load extension {}\n{}'.format(extension, exc))
 
 #token = os.environ['AndroBotKey']
 token1 = sys.argv[1]
-
 chatbot = ChatBot(
   'AndroBot',
   trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
@@ -23,6 +32,9 @@ async def on_ready():
   print('Logged in as: ' + client.user.name + ' ' + client.user.id)
   await client.change_presence(game=discord.Game(name='Use "?" as prefix | ?info | ' + str(len(client.servers)) + ' guilds'))
 
+class Main_Commands():
+  def __init__(self,client):
+    self.client=client
 
 @client.command()
 async def ping(): 
@@ -44,9 +56,5 @@ async def clear(ctx, amount=0):
       messages.append(message)
     await client.delete_messages(messages)
     await client.say("Messages Cleared")
-
-@client.command()
-async def info():
-  await client.say('AndroBot is a pet project by @AndroStudios#8686. The prefix is "?". The following commands are available: "ai", "ping", and "info")
 
 client.run(token1)
