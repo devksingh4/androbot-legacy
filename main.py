ca71d7ed08037d44407fa890eb8cd3c40f378f49
Dev Singh <dev@devksingh.com>
2020-11-09 20:04:56 -0600

    h

--- main.py
+++ main.py
@@ -78,8 +78,8 @@ async def meme(ctx, numMemes=None):
         meme_options = [i for i in meme_options if not i.stickied]
         for i in randomlist:
           selectedpost = meme_options[i]
-          e = discord.Embed(title="Random meme").set_image(url=selectedpost.url)
-          await ctx.send("Here is a random meme: ", embed=e)
+        e = discord.Embed(title="Random meme").set_image(url=selectedpost.url)
+        await ctx.send("Here is a random meme: ", embed=e)
         x -= 1
 
 client.run(token)
